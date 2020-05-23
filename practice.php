f4a34c02ab69fa85d6688b36de43aecc76686fb1
Richard Ogujawa <richardogujawa7@gmail.com>
2020-05-24 00:31:43 +0100

    added some variables in php for contact form

--- practice.php
+++ practice.php
@@ -1,3 +1,26 @@
+<?php
+//Message Vars
+$msg = ''; //This is the message that will be outputed, it's emtpy because we fill it with text when we need it (i.e. in the Passed and Failed sections)
+$msgClass = ''; //This allows you to give the message some properties, like red text. It's empty for the same reason as above
+//Check For Submit
+
+if(filter_has_var(INPUT_POST, 'submit')){
+    //Get Form Data 
+    $name = $_POST['name']; //grab the variables from the form
+    $email = $_POST['email']; 
+    $message = $_POST['message']; 
+
+    //Check Required Fields
+    if(!empty($email) && !empty($name) && !empty($message)) {
+        //Passed 
+    } else {
+        //Failed
+
+        $msg = 'Please fill in all fields'; //message outputed if the form isn't filled out
+        $msgClass = 'alert-danger'; //we'll use this class to make the text red in css
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -22,12 +45,31 @@
 
 <body>
     <nav class="navbar navbar-default">
-        <div class="container">
+        <div class="container notThis">
+
             <div class="navbar-header">
                 <a class="navbar-brand" href="practice.php">My Website </a>
             </div>
         </div>
     </nav>
-</body>
 
+    <div class="container">
+        <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>"> <!--PHP_SELF just means that it echoes to itself-->
+            <div class="form-group">
+                <label>Name</label>
+                <input type="text" name="name" class="form-control" value="">
+            </div>
+            <div class="form-group">
+                <label>Email</label>
+                <input type="text" name="" class="form-control" value="">
+            </div>
+            <div class="form-group">
+                <label>Message</label>
+                <textarea name="message" class="form-control"></textarea>
+            </div>
+            <br>
+            <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+</body>
 </html>
